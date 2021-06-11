f9c0aa728974c3a31443eedbe0efb780a80b9d63
Hendrik Schaeidt <he.schaeidt@gmail.com>
2021-06-11 13:16:56 +0200

    Initial Commit

---
+++ Shared/SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitApp.swift
+//  Shared
+//
+//  Created by Hendrik Schaeidt on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
